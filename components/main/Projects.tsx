5e2787663fe3a93078c6c763b1f8d9c4729cc5f1
yaiser.avila <yaiser.avila@triggle.app>
2024-04-22 00:08:37 +0200

    Yaiser: fix projects

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -12,19 +12,19 @@ const Projects = () => {
       </h1>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
         <ProjectCard
-          src="/NextWebsite.png"
-          title="Modern Next.js Portfolio"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          src="/second_project.webp"
+          title="EKS ZeroDowntime Upgrader"
+          description="The EKS ZeroDowntime Upgrader is a robust tool designed to manage updates and upgrades within Amazon Elastic Kubernetes Service (EKS) clusters without causing any service interruptions. This project aims to automate the process of applying version upgrades, node group updates, and configuration changes seamlessly, ensuring that applications continue to operate smoothly during the transition."
         />
         <ProjectCard
-          src="/CardImage.png"
-          title="Interactive Website Cards"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          src="/third_project.webp"
+          title="ConcurrentFlow Deployer"
+          description="ConcurrentFlow Deployer is designed to streamline and accelerate the CI/CD pipeline using advanced concurrency models and Docker's built-in caching mechanisms. This project aims to reduce build and deployment times significantly by optimizing resource utilization and minimizing rebuilds."
         />
         <ProjectCard
-          src="/SpaceWebsite.png"
-          title="Space Themed Website"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          src="/first_project.webp"
+          title="IaC EnviroProvisioner"
+          description="The IaC EnviroProvisioner is a sophisticated tool designed to automate the provisioning and management of computing environments using Infrastructure as Code (IaC) methodologies. This project utilizes Terraform, Terragrunt, and Spacelift, targeting the AWS platform to streamline the setup, scaling, and maintenance of infrastructure, ensuring consistent and repeatable deployments."
         />
       </div>
     </div>
