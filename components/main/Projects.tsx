8d29779e80cfd08e9452c8350166b44fa5237ebb
yaiser.avila <yaiser.avila@triggle.app>
2024-05-02 19:43:14 +0200

    SRE: add company name

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -8,10 +8,11 @@ const Projects = () => {
       id="projects"
     >
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
-        My Projects
+        Flagship projects
       </h1>
       <div className="h-full w-full flex flex-col md:flex-wrap md:flex-row gap-10 px-10">
         <ProjectCard
+          company="Triggle Spain SLU"
           src="/leadership.webp"
           title="EKS Quantic Update and Leadership"
           description="I oversaw the update of EKS clusters from version 1.24 to 1.29, which reduced the costs of extended support by 82.5% due to more efficient use of autoscaling groups. Additionally, I have been managing a team of three engineers in automating key infrastructure components"
@@ -21,6 +22,7 @@ const Projects = () => {
           outdescription="82.5% reduction in extended support costs due to cluster updates -> Enhanced system scalability and reliability"
         />
         <ProjectCard
+          company="Triggle Spain SLU"
           src="/second_project.webp"
           title="CI/CD Automation and Deployment Acceleration"
           description="Focusing on improving operational efficiency, I spearheaded the automation of the continuous integration and continuous deployment (CI/CD) pipelines, enhance the build time using concurrency and docker caching. This project involved overhauling existing deployment methodologies and introducing automation scripts that streamlined the deployment process."
@@ -30,6 +32,7 @@ const Projects = () => {
           outdescription="Deployment and build times reduced by 500% -> Enhanced consistency and reliability in deployments"
         />
         <ProjectCard
+          company="Triggle Spain SLU"
           src="/boats.webp"
           title="Tourism Platform-as-a-Service (PaaS) Cost Optimization"
           description="In my role at Triggle SLU, a cloud-native company serving the tourism sector, I led a major initiative to reduce platform costs across AWS accounts. By implementing targeted optimizations and refining resource usage, I achieved a 27% reduction in overall platform costs. Key strategies included enhancing autoscaling capabilities, apply cleanup policies, automate cleanups using crons and revising our resource allocation to better fit usage patterns."
--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React from "react";
 
 interface Props {
+  company: string;
   src: string;
   title: string;
   description: string;
@@ -11,7 +12,7 @@ interface Props {
   outdescription: string;
 }
 
-const ProjectCard = ({ src, title, description, techtitle, techdescription, outtitle, outdescription }: Props) => {
+const ProjectCard = ({ company, src, title, description, techtitle, techdescription, outtitle, outdescription }: Props) => {
   return (
     <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] projectCard">
       <Image
@@ -24,6 +25,7 @@ const ProjectCard = ({ src, title, description, techtitle, techdescription, outt
 
       <div className="relative p-4">
         <h1 className="text-2xl font-semibold text-white">{title}</h1>
+        <h1 className="text-xl font-semibold text-white">{company}</h1>
         <p className="mt-2 text-gray-300">{description}</p>
         <h2 className="mt-2 text-xl font-bold text-white">{outtitle}</h2>
         <p className="mt-1 text-l text-purple-300">{outdescription}</p>
