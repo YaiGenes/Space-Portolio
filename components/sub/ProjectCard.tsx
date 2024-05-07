7d56d2c6b2a3fffead3bd8a05760e8a4d09d3fda
yaiser.avila <yaiser.avila@triggle.app>
2024-05-07 17:51:33 +0200

    SRE: upload new projects

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -5,14 +5,17 @@ interface Props {
   company: string;
   src: string;
   title: string;
+  soltitle: string;
   description: string;
+  soldescription: string;
   techtitle: string;
   outtitle: string;
   techdescription: string;
   outdescription: string;
+  desctitle: string
 }
 
-const ProjectCard = ({ company, src, title, description, techtitle, techdescription, outtitle, outdescription }: Props) => {
+const ProjectCard = ({ company, src, title, soltitle, description, soldescription, techtitle, techdescription, outtitle, outdescription, desctitle }: Props) => {
   return (
     <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] projectCard">
       <Image
@@ -26,7 +29,10 @@ const ProjectCard = ({ company, src, title, description, techtitle, techdescript
       <div className="relative p-4">
         <h1 className="text-2xl font-semibold text-white">{title}</h1>
         <h1 className="text-xl font-semibold text-white">{company}</h1>
+        <h2 className="mt-2 text-xl font-bold text-white">{desctitle}</h2>
         <p className="mt-2 text-gray-300">{description}</p>
+        <h2 className="mt-2 text-xl font-bold text-white">{soltitle}</h2>
+        <p className="mt-2 text-gray-300">{soldescription}</p>
         <h2 className="mt-2 text-xl font-bold text-white">{outtitle}</h2>
         <p className="mt-1 text-l text-purple-300">{outdescription}</p>
         <h3 className="mt-2 text-l font-semibold text-white">{techtitle}</h3>
